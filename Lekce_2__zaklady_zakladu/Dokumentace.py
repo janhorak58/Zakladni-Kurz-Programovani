f90ed7bc1b4fb92c1a402019c485116db04d0e71
janhorak58 <janhorak58@gmail.com>
2021-12-14 18:38:57 +0100

    Lekce 2 - Základy Základů

---
+++ Lekce_2__zaklady_zakladu/Dokumentace.py
@@ -0,0 +1,32 @@
+# Toto je Komentář - poznáš ho podle # na začátku řádku
+
+# PROMĚNNÉ 
+# Proměnné jsou v podstatě takové nosiče informací. Do těchto nosičů uložíme informaci takto
+x = 1
+# Nejprve napíšeme název proměnné, potom = a nakonec proměnné přiřadíme nějakou hodnotu - třeba 1
+
+# Názvy proměnných mají svá Pravidla - Musí to být jedno slovo a nesmí obsahovat žádné Speciální znaky (*<-"/# atd.), ale může obsahovat malá, velká písmena, čísla, nebo podtržítko(_)
+
+# x_1 ANO
+# x - 2 NE
+
+
+# ZÁKLADNÍ FUNKCE
+# Funkci poznáš tak, že má za sebou závorky () a do těch závorek se většinou něco píše.
+
+# Nejzákladnější funkcí je určitě funkce print()
+# Do závorek píšeme to co chceme vytisknou uživateli na obrazovku
+# Například Když chci vytisknout hodnotu dříve deklarované proměnné x udělám to takto:
+print(x)
+
+
+# Co když chceme ale vytisknout nějaký text?
+# I to je možné a je to velmi jednoduché...
+# Do Závorek napíšeme text, který chceme, ale musíme ho vložit do uvozovek "Text" - Takto:
+print("Toto je 2. řádek")
+
+# Uvozovky se dají napsat jako SHIFT + klávesa o 1 napravo od L
+
+# Když chceme zkombinovat Text a hodnotu proměnné oddělíme vše čárkou - takto:
+print("Toto je hodnota Proměnné x: ", x)
+
---
+++ Lekce_2__zaklady_zakladu/cviceni.py
@@ -0,0 +1,11 @@
+"""
+    Vytvoř Proměnnou y a přiřaď ji hodnotu 27
+    
+    Potom vytiskni větu: Sněhurka a (Hodnota x) Trpaslíků
+"""
+    
+y = 27
+print("Sněhurka a ", y, " Trpaslíků")
+
+# Otevřít Terminál   ==> CTRL + ;
+# Rozběhnout program ==> python {název souboru}.py
